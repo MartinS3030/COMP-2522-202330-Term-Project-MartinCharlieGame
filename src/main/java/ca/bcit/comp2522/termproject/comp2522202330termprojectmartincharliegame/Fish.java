fd1ae8fcb892dcc455e94b512d82b97f5ff7af58
MARTIN SIU <msiu28@my.bcit.ca>
2023-11-23 14:49:11 -0800

    Create fish class and add accessors and constructor

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Fish.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Fish.java
@@ -1,4 +1,37 @@
 package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
 
 public class Fish {
+    private String name;
+    private String rarity;
+    private String requirementType;
+    private int requirementValue;
+    private double value;
+
+    public Fish(final String name, final String rarity, final String requirementType, final int requirementValue, final double value) {
+        this.name = name;
+        this.rarity = rarity;
+        this.requirementType = requirementType;
+        this.requirementValue = requirementValue;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public String getRequirementType() {
+        return requirementType;
+    }
+
+    public int getRequirementValue() {
+        return requirementValue;
+    }
+
+    public double getValue() {
+        return value;
+    }
 }
