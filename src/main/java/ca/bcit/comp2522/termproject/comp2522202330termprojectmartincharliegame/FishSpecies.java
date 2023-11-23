db546ebcdb05f641060bb8c77feb609a66b23ff7
MARTIN SIU <msiu28@my.bcit.ca>
2023-11-23 14:49:41 -0800

    Create FishSpecies class with populate method

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/FishSpecies.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/FishSpecies.java
@@ -1,4 +1,47 @@
 package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
 
-public enum FishSpecies {
+import java.util.HashMap;
+
+public class FishSpecies {
+    private final HashMap<Integer, Fish> fishSpecies = new java.util.HashMap<>();
+
+    public FishSpecies() {
+        populateFishSpecies();
+    }
+
+    public void populateFishSpecies() {
+        fishSpecies.put(1, new Fish("Bass", "Common", "Water", 1, 1.0));
+        fishSpecies.put(2, new Fish("Catfish", "Common", "Water", 1, 1.0));
+        fishSpecies.put(3, new Fish("Char", "Common", "Water", 1, 1.0));
+        fishSpecies.put(4, new Fish("Clownfish", "Common", "Water", 1, 1.0));
+        fishSpecies.put(5, new Fish("Goldfish", "Common", "Water", 1, 1.0));
+        fishSpecies.put(6, new Fish("Guppy", "Common", "Water", 1, 1.0));
+        fishSpecies.put(7, new Fish("Koi", "Common", "Water", 1, 1.0));
+        fishSpecies.put(8, new Fish("Piranha", "Common", "Water", 1, 1.0));
+        fishSpecies.put(9, new Fish("Red Snapper", "Common", "Water", 1, 1.0));
+        fishSpecies.put(10, new Fish("Salmon", "Common", "Water", 1, 1.0));
+        fishSpecies.put(11, new Fish("Sweetfish", "Common", "Water", 1, 1.0));
+        fishSpecies.put(12, new Fish("Turtle", "Common", "Water", 1, 1.0));
+        fishSpecies.put(13, new Fish("Yellow Perch", "Common", "Water", 1, 1.0));
+
+        fishSpecies.put(14, new Fish("Arapaima", "Rare", "Water", 1, 1.0));
+        fishSpecies.put(15, new Fish("Coelacanth", "Rare", "Water", 1, 1.0));
+        fishSpecies.put(16, new Fish("Dorado", "Rare", "Water", 1, 1.0));
+        fishSpecies.put(17, new Fish("Marlin", "Rare", "Water", 1, 1.0));
+        fishSpecies.put(18, new Fish("Saw Shark", "Rare", "Water", 1, 1.0));
+        fishSpecies.put(19, new Fish("Shark", "Rare", "Water", 1, 1.0));
+        fishSpecies.put(20, new Fish("Sturgeon", "Rare", "Water", 1, 1.0));
+        fishSpecies.put(21, new Fish("Tuna", "Rare", "Water", 1, 1.0));
+
+        fishSpecies.put(22, new Fish("Bruce", "Legendary", "Water", 1, 1.0));
+        fishSpecies.put(23, new Fish("Dory", "Legendary", "Water", 1, 1.0));
+        fishSpecies.put(24, new Fish("Flounder", "Legendary", "Water", 1, 1.0));
+        fishSpecies.put(25, new Fish("Mr. Krabs", "Legendary", "Water", 1, 1.0));
+        fishSpecies.put(26, new Fish("Mrs. Puff", "Legendary", "Water", 1, 1.0));
+        fishSpecies.put(27, new Fish("Nemo", "Legendary", "Water", 1, 1.0));
+    }
+
+    public Fish getFish(int key) {
+        return fishSpecies.get(key);
+    }
 }
