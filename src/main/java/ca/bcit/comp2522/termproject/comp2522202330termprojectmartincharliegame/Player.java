47bfd5ee87d040a85e243af7c280f6342d25aada
MARTIN SIU <msiu28@my.bcit.ca>
2023-11-23 20:11:01 -0800

    Add singleton design to Player

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Player.java
@@ -1,4 +1,23 @@
 package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
 
+import java.util.ArrayList;
+
 public class Player {
+    private static Player instance;
+    private final String name;
+    private ArrayList<Reward> inventory = new ArrayList<>();
+//    private Fishing_Rod rod = new Fishing_Rod();
+
+    private Player(String name) {
+        this.name = name;
+    }
+
+    public static Player getInstance(String name) {
+        if (instance == null) {
+            instance = new Player(name);
+        }
+        return instance;
+    }
+
+
 }
