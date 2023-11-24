e6ab6755482c736c024714f07121513c59454b9e
MARTIN SIU <msiu28@my.bcit.ca>
2023-11-23 19:59:20 -0800

    Add start method to FishDisplay

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/FishDisplay.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/FishDisplay.java
@@ -11,6 +11,45 @@ import java.util.Random;
 import javafx.scene.layout.HBox;
 
 public class FishDisplay extends Application {
+    private ArrayList<Fish> fishList = new ArrayList<>();
+    private Random random = new Random();
+
+    @Override
+    public void start(Stage primaryStage) {
+        Image oceanImage = new Image("file:../../resources/Ocean.png");
+        ImageView oceanImageView = new ImageView(oceanImage);
+
+        generateFish();
+        for (Fish fish : fishList) {
+            System.out.println(fish.getName());
+        }
+
+        HBox fishContainer = new HBox();
+        fishContainer.setSpacing(70);
+
+        for (Fish fish : fishList) {
+            Image fishImage = new Image("file:../../resources/Fish/" + fish.getName() + ".png");
+            ImageView fishImageView = new ImageView(fishImage);
+
+            fishImageView.setFitWidth(100);
+            fishImageView.setFitHeight(100);
+
+            fishImageView.setTranslateY(50);
+            fishImageView.setTranslateX(200);
+
+            fishContainer.getChildren().add(fishImageView);
+        }
+
+        StackPane root = new StackPane();
+        root.getChildren().addAll(oceanImageView, fishContainer);
+
+        Scene scene = new Scene(root, 1200, 648);
+        primaryStage.setTitle("Fish Display");
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
 
 
     public static void main(String[] args) {
