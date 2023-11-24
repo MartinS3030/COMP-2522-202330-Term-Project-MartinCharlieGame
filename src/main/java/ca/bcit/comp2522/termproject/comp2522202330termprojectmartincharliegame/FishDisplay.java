8d5aae9411865e32bee068472dc63e4cc3cd94ed
MARTIN SIU <msiu28@my.bcit.ca>
2023-11-23 19:58:52 -0800

    Create FishDisplay class

---
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/FishDisplay.java
@@ -0,0 +1,19 @@
+package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+import java.util.Random;
+import javafx.scene.layout.HBox;
+
+public class FishDisplay extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
