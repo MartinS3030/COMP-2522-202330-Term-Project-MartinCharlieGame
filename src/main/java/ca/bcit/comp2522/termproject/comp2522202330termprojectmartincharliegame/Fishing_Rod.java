7155b897d75012609137972d2ad5bf97b066bab9
cz1501 <122151441+cz1501@users.noreply.github.com>
2023-11-25 03:02:27 -0800

    Create default constructor

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Fishing_Rod.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Fishing_Rod.java
@@ -7,8 +7,23 @@ public class Fishing_Rod {
 
     public Fishing_Rod(Rod_Components rodComponent1, Rod_Components rodComponent2, Rod_Components rodComponent3,
                        Rod_Components rodComponent4, Rod_Components rodComponent5) {
-//        populateRodComponents();
+        Components.add(rodComponent1);
+        Components.add(rodComponent2);
+        Components.add(rodComponent3);
+        Components.add(rodComponent4);
+        Components.add(rodComponent5);
     }
 
+    public Fishing_Rod() {
+        Components.add(new Rod_Components());
+        Components.add(new Rod_Components());
+        Components.add(new Rod_Components());
+        Components.add(new Rod_Components());
+        Components.add(new Rod_Components());
+    }
+
+    public ArrayList<Rod_Components> getComponents() {
+        return Components;
+    }
 
 }
