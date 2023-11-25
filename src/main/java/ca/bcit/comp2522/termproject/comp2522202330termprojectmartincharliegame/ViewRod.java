1f560af5fe29bfab04afdc829e8d7bd64733a192
MARTIN SIU <msiu28@my.bcit.ca>
2023-11-24 20:05:13 -0800

    Create ViewRod class

---
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/ViewRod.java
@@ -0,0 +1,13 @@
+package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class ViewRod extends Application {
+
+}
