41edb4b5bc78f55fe04ded1636a958b432d54d78
MARTIN SIU <msiu28@my.bcit.ca>
2023-11-24 20:04:09 -0800

    Modify ButtonMaker to set position

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/ButtonMaker.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/ButtonMaker.java
@@ -6,18 +6,20 @@ import javafx.event.EventHandler;
 
 public class ButtonMaker {
 
-    public static Button createButton(String label, EventHandler<ActionEvent> eventHandler) {
+    public static Button createButton(String label, EventHandler<ActionEvent> eventHandler, double xPosition, double yPosition) {
         Button button = new Button(label);
 
-        button.setPrefWidth(150);
+        button.setPrefWidth(200);
         button.setPrefHeight(75);
         button.setOpacity(0.5);
 
-        button.setStyle("-fx-border-color: black; -fx-border-width: 2px; -fx-background-color: lightblue; -fx-border-radius: 10px; -fx-font-size: 20px; -fx-text-fill: black; -fx-font-weight: bold;");
+        button.setStyle("-fx-border-color: black; -fx-border-width: 2px; -fx-background-color: lightblue; -fx-border-radius: 10px; -fx-font-size: 15px; -fx-text-fill: black; -fx-font-weight: bold;");
+
+        button.setLayoutX(xPosition);
+        button.setLayoutY(yPosition);
 
         button.setOnAction(eventHandler);
 
         return button;
     }
 }
-
