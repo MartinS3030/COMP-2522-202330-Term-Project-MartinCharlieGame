78e02f407056ed26c0bda95f97a9ac283f20de2a
MARTIN SIU <msiu28@my.bcit.ca>
2023-11-28 23:00:54 -0800

    Create BulletinBoard class

---
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/BulletinBoard.java
@@ -0,0 +1,8 @@
+package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class BulletinBoard implements Board{
+
+}
