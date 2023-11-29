88d2972843eaebe7e6e49202f2f699e4023e0e3b
MARTIN SIU <msiu28@my.bcit.ca>
2023-11-28 23:01:50 -0800

    Create singleton design in BulletinBoard

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/BulletinBoard.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/BulletinBoard.java
@@ -4,5 +4,25 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class BulletinBoard implements Board{
+    private static BulletinBoard instance;
+    private final ArrayList<Quest> quests = new ArrayList<>();
+    private final Player player = Player.getInstance("Charlie");
+    private final Random random = new Random();
+
+    private BulletinBoard() {
+        generateQuests();
+    }
+
+    /**
+     * Gets the instance of the bulletin board.
+     *
+     * @return the instance of the bulletin board
+     */
+    public static BulletinBoard getInstance() {
+        if (instance == null) {
+            instance = new BulletinBoard();
+        }
+        return instance;
+    }
 
 }
