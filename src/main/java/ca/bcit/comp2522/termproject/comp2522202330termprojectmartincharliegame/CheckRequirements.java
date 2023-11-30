b3c0ddc9c6e14b869dda210b8d0cf8be1c729279
MARTIN SIU <msiu28@my.bcit.ca>
2023-11-29 19:16:12 -0800

    Create methods to check requirements for dice

---
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/CheckRequirements.java
@@ -0,0 +1,139 @@
+package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckRequirements {
+    public boolean greaterThan(ArrayList<Dice> diceList, int value) {
+        int sum = 0;
+        for (Dice dice : diceList) {
+            sum += dice.getFaceUpValue();
+        }
+        return sum > value;
+    }
+
+    public boolean lessThan(ArrayList<Dice> diceList, int value) {
+        int sum = 0;
+        for (Dice dice : diceList) {
+            sum += dice.getFaceUpValue();
+        }
+        return sum < value;
+    }
+
+    public boolean equalTo(ArrayList<Dice> diceList, int value) {
+        int sum = 0;
+        for (Dice dice : diceList) {
+            sum += dice.getFaceUpValue();
+        }
+        return sum == value;
+    }
+
+    public boolean ofAKind(ArrayList<Dice> diceList, int value) {
+        int[] faceUpValues = new int[6];
+        for (Dice dice : diceList) {
+            faceUpValues[dice.getFaceUpValue() - 1]++;
+        }
+        for (int i = 0; i < faceUpValues.length; i++) {
+            if (faceUpValues[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean straight(ArrayList<Dice> diceList, int value) {
+        int[] faceUpValues = new int[6];
+
+        for (Dice dice : diceList) {
+            faceUpValues[dice.getFaceUpValue() - 1]++;
+        }
+
+        for (int i = 0; i <= faceUpValues.length - value; i++) {
+            boolean consecutive = true;
+            for (int j = 0; j < value; j++) {
+                if (faceUpValues[i + j] == 0) {
+                    consecutive = false;
+                    break;
+                }
+            }
+            if (consecutive) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+    public boolean fullHouse(ArrayList<Dice> diceList) {
+        int[] faceUpValues = new int[6];
+        for (Dice dice : diceList) {
+            faceUpValues[dice.getFaceUpValue() - 1]++;
+        }
+        boolean threeOfAKind = false;
+        boolean twoOfAKind = false;
+        for (int i = 0; i < faceUpValues.length; i++) {
+            if (faceUpValues[i] == 3) {
+                threeOfAKind = true;
+            }
+            if (faceUpValues[i] == 2) {
+                twoOfAKind = true;
+            }
+        }
+        return threeOfAKind && twoOfAKind;
+    }
+
+    public boolean oddsOnly(ArrayList<Dice> diceList) {
+        for (Dice dice : diceList) {
+            if (dice.getFaceUpValue() % 2 == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean evensOnly(ArrayList<Dice> diceList) {
+        for (Dice dice : diceList) {
+            if (dice.getFaceUpValue() % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean twoPair(ArrayList<Dice> diceList) {
+        int[] faceUpValues = new int[6];
+        for (Dice dice : diceList) {
+            faceUpValues[dice.getFaceUpValue() - 1]++;
+        }
+        int pairs = 0;
+        for (int i = 0; i < faceUpValues.length; i++) {
+            if (faceUpValues[i] == 2) {
+                pairs++;
+            }
+        }
+        return pairs == 2;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Dice> diceList = new ArrayList<Dice>();
+        for (int i = 0; i < 5; i++) {
+            diceList.add(new Rod_Components());
+        }
+//        print dice values
+        for (Dice dice : diceList) {
+            System.out.println(dice.getFaceUpValue());
+        }
+        CheckRequirements checkRequirements = new CheckRequirements();
+        System.out.println("ofAKind: " + checkRequirements.ofAKind(diceList, 3));
+        System.out.println("straight: " + checkRequirements.straight(diceList, 3));
+        System.out.println("full house: " + checkRequirements.fullHouse(diceList));
+        System.out.println("odds only: " + checkRequirements.oddsOnly(diceList));
+        System.out.println("evens only: " + checkRequirements.evensOnly(diceList));
+        System.out.println("greater than: " + checkRequirements.greaterThan(diceList, 10));
+        System.out.println("less than: " + checkRequirements.lessThan(diceList, 10));
+        System.out.println("equal to: " + checkRequirements.equalTo(diceList, 15));
+        System.out.println("two pair: " + checkRequirements.twoPair(diceList));
+    }
+}
