27b70aa24b0788c97c066fb6a1f78e242d1ea14a
MARTIN SIU <msiu28@my.bcit.ca>
2023-12-06 14:17:26 -0800

    Create shop class

---
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Shop.java
@@ -0,0 +1,12 @@
+package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
+
+import java.util.ArrayList;
+
+public class Shop {
+    ArrayList<Item> items = new ArrayList<>();
+
+    public Shop() {
+
+    }
+
+}
