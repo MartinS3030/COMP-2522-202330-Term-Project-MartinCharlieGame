ae646649870309054858e1f25f65da748f0b6045
MARTIN SIU <msiu28@my.bcit.ca>
2023-12-06 13:56:55 -0800

    Add VillageDisplay class

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/ButtonMaker.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/ButtonMaker.java
@@ -3,6 +3,7 @@ package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.text.Font;
 
 public class ButtonMaker {
 
@@ -11,9 +12,11 @@ public class ButtonMaker {
 
         button.setPrefWidth(200);
         button.setPrefHeight(75);
-        button.setOpacity(0.5);
-
-        button.setStyle("-fx-border-color: black; -fx-border-width: 2px; -fx-background-color: lightblue; -fx-border-radius: 10px; -fx-font-size: 15px; -fx-text-fill: black; -fx-font-weight: bold;");
+        button.setOpacity(0.7);
+        Font font = Font.loadFont("file:resources/Fonts/PressStart2P-Regular.ttf", 10);
+        button.setFont(font);
+        button.setWrapText(true);
+        button.setStyle("-fx-border-color: black; -fx-border-width: 2px; -fx-background-color: lightblue; -fx-border-radius: 10px;");
 
         button.setLayoutX(xPosition);
         button.setLayoutY(yPosition);
---
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/VillageDisplay.java
@@ -0,0 +1,60 @@
+package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
+
+import javafx.animation.FadeTransition;
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+
+import javafx.event.ActionEvent;
+import javafx.util.Duration;
+
+public class VillageDisplay extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Image village = new Image("file:../../resources/village.png");
+        ImageView villageView = new ImageView(village);
+        villageView.setFitHeight(648);
+        villageView.setFitWidth(1200);
+
+        Button bulletinBoard = ButtonMaker.createButton("Bulletin Board", this::showBulletinBoard, 0, 0);
+        Button shop = ButtonMaker.createButton("Shop", this::showShop, 0, 0);
+        Button viewQuests = ButtonMaker.createButton("View Quests", this::showBulletinBoard, 0, 0);
+        Button viewInventory = ButtonMaker.createButton("View Inventory", this::showBulletinBoard, 0, 0);
+        Button sleep = ButtonMaker.createButton("Sleep", this::showSleep, 0, 0);
+        Button buyBoat = ButtonMaker.createButton("Buy Boat", this::showBulletinBoard, 0, 0);
+        Button save = ButtonMaker.createButton("Save", this::showSave, 0, 0);
+
+        VBox buttonBox = new VBox(bulletinBoard, shop, viewQuests, viewInventory, sleep, buyBoat, save);
+        buttonBox.setAlignment(javafx.geometry.Pos.CENTER);
+        buttonBox.setSpacing(10);
+
+        StackPane root = new StackPane(villageView, buttonBox);
+        Scene scene = new Scene(root, 1200, 648);
+        primaryStage.setTitle("Village");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public void showBulletinBoard(ActionEvent event) {
+
+    }
+
+    public void showShop(ActionEvent event) {
+        System.out.println("Shop");
+    }
+
+    public void showSleep(ActionEvent event) {
+        System.out.println("Sleep");
+    }
+
+    public void showSave(ActionEvent event) {
+        System.out.println("Save");
+    }
+}
