1058384b3951642d7c5f217b0a8bfd43fea0f8bb
MARTIN SIU <msiu28@my.bcit.ca>
2023-12-07 17:23:48 -0800

    Add javadocs and fix checkstyle warnings

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Quest.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Quest.java
@@ -2,8 +2,18 @@ package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
 
 import java.io.Serializable;
 
+/**
+ * Represents a quest that the player can complete.
+ *
+ * @author Martin Siu, Charlie Zhang
+ * @version 2023
+ */
 public class Quest implements Serializable {
-    private int id;
+    /**
+     * The multiplier used to generate the ID of the quest.
+     */
+    public static final int ID_MULTIPLIER = 1000000;
+    private final int id;
     private final String title;
     private final String giver;
     private final int reward;
@@ -12,7 +22,19 @@ public class Quest implements Serializable {
     private final int objectiveAmount;
     private final String description;
 
-    public Quest(String title, String giver, int reward, Fish objective, int difficulty, int objectiveAmount, String description) {
+    /**
+     * Constructs a quest.
+     *
+     * @param title the title of the quest
+     * @param giver the name of the NPC that gives the quest
+     * @param reward the amount of money the player gets for completing the quest
+     * @param objective the type of fish the player needs to catch
+     * @param difficulty the difficulty of the quest
+     * @param objectiveAmount the amount of fish the player needs to catch
+     * @param description the description of the quest
+     */
+    public Quest(final String title, final String giver, final int reward, final Fish objective, final int difficulty,
+                 final int objectiveAmount, final String description) {
         this.id = generateID();
         this.title = title;
         this.giver = giver;
@@ -23,39 +45,84 @@ public class Quest implements Serializable {
         this.description = description;
     }
 
+    /**
+     * Returns the title of the quest.
+     *
+     * @return the title of the quest as a String
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Returns the description of the quest.
+     *
+     * @return the description of the quest as a String
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Returns the reward of the quest.
+     *
+     * @return the reward of the quest as an int
+     */
     public int getReward() {
         return reward;
     }
 
+    /**
+     * Returns the objective of the quest.
+     *
+     * @return the objective of the quest as a Fish
+     */
     public Fish getObjective() {
         return objective;
     }
 
+    /**
+     * Returns the amount of fish the player needs to catch.
+     *
+     * @return the amount of fish the player needs to catch as an int
+     */
     public int getObjectiveAmount() {
         return objectiveAmount;
     }
 
+    /**
+     * Returns the name of the NPC that gives the quest.
+     *
+     * @return the name of the NPC that gives the quest as a String
+     */
     public String getGiver() {
         return giver;
     }
 
+    /**
+     * Returns the difficulty of the quest.
+     *
+     * @return the difficulty of the quest as an int
+     */
     public int getDifficulty() {
         return difficulty;
     }
 
+    /**
+     * Returns the ID of the quest.
+     *
+     * @return the ID of the quest as an int
+     */
     public int getID() {
         return id;
     }
 
+    /**
+     * Generates a random ID for the quest.
+     *
+     * @return the ID of the quest as an int
+     */
     public int generateID() {
-        return (int) (Math.random() * 1000000);
+        return (int) (Math.random() * ID_MULTIPLIER);
     }
 }
