8a50be5f4e8ecbe09e205fbf142e4c659894889a
MARTIN SIU <msiu28@my.bcit.ca>
2023-12-07 18:19:49 -0800

    Add javadocs and fix checkstyle warnings

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Board.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Board.java
@@ -2,7 +2,21 @@ package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a Board.
+ *
+ * @author Martin Siu, Charlie Zhang
+ * @version 2023
+ */
 public interface Board {
+    /**
+     * Generates Quests.
+     */
     void generateQuests();
+    /**
+     * Gets the Quests.
+     *
+     * @return the Quests in an ArrayList
+     */
     ArrayList<Quest> getQuests();
 }
