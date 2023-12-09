4e99866d8afedaaa71dd3aa5260f90c778fc88d9
MARTIN SIU <msiu28@my.bcit.ca>
2023-12-09 13:11:44 -0800

    Add javadocs and fix checkstyle warnings

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/DiceDisplay.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/DiceDisplay.java
@@ -1,12 +1,23 @@
 package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
 
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-
 import java.util.ArrayList;
 
+/**
+ * A class that displays the dice.
+ *
+ * @author Charlie Zhang & Martin Siu
+ * @version 2023
+ */
 public class DiceDisplay {
     private final Color BORDER_COLOR = Color.BLACK;
     private final Color SELECTED_COLOR = Color.FORESTGREEN;
@@ -15,11 +26,13 @@ public class DiceDisplay {
     private final Fishing_Rod fishingRod;
     private final HBox hBox;
     private final VBox[] vBox;
-//    private final Stage primaryStage;
     private final Player player;
     private final ArrayList<Dice> selectedDice;
     private final ArrayList<Dice> usedDice;
 
+    /**
+     * Creates a new DiceDisplay.
+     */
     public DiceDisplay() {
         player = Player.getInstance("Charlie");
         fishingRod = generateRod();
@@ -34,112 +47,191 @@ public class DiceDisplay {
         usedDice = new ArrayList<Dice>();
     }
 
+    /**
+     * Gets the border color.
+     *
+     * @return the border color
+     */
     public Color getBORDER_COLOR() {
         return BORDER_COLOR;
     }
 
+    /**
+     * Gets the selected color.
+     *
+     * @return the selected color
+     */
     public Color getSELECTED_COLOR() {
         return SELECTED_COLOR;
     }
 
-    public Color getUSED_COLOR() {
-        return USED_COLOR;
-    }
-
+    /**
+     * Gets the dice views.
+     *
+     * @return the dice views
+     */
     public ImageView[] getDiceViews() {
         return diceViews;
     }
 
+    /**
+     * Gets the fishing rod.
+     *
+     * @return the fishing rod
+     */
     public Fishing_Rod getFishingRod() {
         return fishingRod;
     }
 
+    /**
+     * Gets the HBox.
+     *
+     * @return the HBox
+     */
     public HBox gethBox() {
         return hBox;
     }
 
+    /**
+     * Gets the VBox.
+     *
+     * @return the VBox
+     */
     public VBox[] getvBox() {
         return vBox;
     }
 
+    /**
+     * Gets the player.
+     *
+     * @return the player
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * Gets the selected dice.
+     *
+     * @return the selected dice
+     */
     public ArrayList<Dice> getUsedDice() {
         return usedDice;
     }
 
+    /**
+     * Gets the selected dice.
+     *
+     * @return the selected dice
+     */
     public ArrayList<Dice> getSelectedDice() {
         return selectedDice;
     }
 
+    /**
+     * Gets the dice display.
+     *
+     * @return the dice display
+     */
     protected Fishing_Rod generateRod() {
         return new Fishing_Rod();
     }
 
-    protected StackPane createRoundedBorderedImageView(ImageView imageView, Color color) {
-        // Create a StackPane to hold the ImageView
+    /**
+     * Creates a rounded bordered ImageView.
+     *
+     * @param imageView the ImageView to create
+     * @param color     the color of the border
+     * @return the StackPane
+     */
+    protected StackPane createRoundedBorderedImageView(final ImageView imageView, final Color color) {
         StackPane stackPane = new StackPane(imageView);
 
         Border border = getRoundedBorder(color);
-
-        // Apply the Border to the StackPane
         stackPane.setBorder(border);
-
-        // Set the size of the StackPane to match the size of the image
         stackPane.setMaxSize(imageView.getFitWidth(), imageView.getFitHeight());
 
         return stackPane;
     }
 
-    private Border getRoundedBorder(Color color) {
-        // Define the corner radii for rounded borders
-        CornerRadii cornerRadii = new CornerRadii(10); // Adjust the radius as needed
-        BorderWidths borderWidth = new BorderWidths(15); // Adjust the width as needed
-
-        // Create a BorderStroke with the desired color, style, width, and rounded corners
+    /**
+     * Gets the rounded border.
+     *
+     * @param color the color
+     * @return the rounded border
+     */
+    private Border getRoundedBorder(final Color color) {
+        CornerRadii cornerRadii = new CornerRadii(10);
+        BorderWidths borderWidth = new BorderWidths(15);
         BorderStroke borderStroke = new BorderStroke(color, BorderStrokeStyle.SOLID, cornerRadii, borderWidth);
-
-        // Create a Border with the specified BorderStroke
         return new Border(borderStroke);
     }
 
-    protected void resetBorderColor(ImageView imageView) {
+    /**
+     * Resets the border color.
+     *
+     * @param imageView the imageView
+     */
+    protected void resetBorderColor(final ImageView imageView) {
         setBorderColor(imageView, BORDER_COLOR);
     }
 
-    protected void setBorderColor(ImageView imageView, Color color) {
-        // Apply the border color to the given ImageView
+    /**
+     * Sets the border color.
+     *
+     * @param imageView the imageView
+     * @param color     the color
+     */
+    protected void setBorderColor(final ImageView imageView, final Color color) {
         Border border = getRoundedBorder(color);
         ((StackPane) imageView.getParent()).setBorder(border);
     }
 
-    protected ImageView getImageViews(Dice dice) {
+    /**
+     * Gets the image views.
+     *
+     * @param dice the dice
+     * @return the image views
+     */
+    protected ImageView getImageViews(final Dice dice) {
         return DiceFace.getDiceFaceImage(dice.getFaceUpValue());
     }
 
-    protected int findDiceViewPosition(ImageView targetImageView) {
+    /**
+     * Finds the dice view position.
+     *
+     * @param targetImageView the target image view
+     * @return the dice view position
+     */
+    protected int findDiceViewPosition(final ImageView targetImageView) {
         for (int i = 0; i < diceViews.length; i++) {
             if (diceViews[i] == targetImageView) {
-                return i; // Found the target ImageView, return its index
+                return i;
             }
         }
-        return -1; // If the target ImageView is not found in the array
+        return -1;
     }
 
+    /**
+     * Updates the dice view.
+     */
     private void updateDiceView() {
         for (int i = 0; i < fishingRod.getComponents().size(); i++) {
             diceViews[i] = getImageViews(fishingRod.getComponents().get(i));
         }
 
         for (ImageView diceView : diceViews) {
-            diceView.setPreserveRatio(true); // Preserve the aspect ratio of the image
-            diceView.setFitWidth(100); // Set the desired width
+            diceView.setPreserveRatio(true);
+            diceView.setFitWidth(100);
         }
     }
 
-    protected void selectDice(int diceIndex) {
+    /**
+     * Selects the dice.
+     *
+     * @param diceIndex the dice index
+     */
+    protected void selectDice(final int diceIndex) {
         System.out.printf("Selecting dice %d\n", diceIndex);
         Dice dice = fishingRod.getComponents().get(diceIndex);
         if (usedDice.contains(dice)) {
@@ -148,7 +240,7 @@ public class DiceDisplay {
         if (selectedDice.contains(dice)) {
             resetBorderColor(diceViews[diceIndex]);
             selectedDice.remove(dice);
-        } else if (!usedDice.contains(dice)){
+        } else if (!usedDice.contains(dice)) {
             for (Dice diceInSelectedDice : selectedDice) {
                 resetBorderColor(diceViews[fishingRod.getComponents().indexOf(diceInSelectedDice)]);
             }
@@ -158,6 +250,11 @@ public class DiceDisplay {
         }
     }
 
+    /**
+     * Gets the dice display.
+     *
+     * @return the dice display
+     */
     public HBox getDiceDisplay() {
         if (!hBox.getChildren().isEmpty()) {
             return hBox;
@@ -173,7 +270,12 @@ public class DiceDisplay {
         return hBox;
     }
 
-    public void addDiceToUsedDice(Dice dice) {
+    /**
+     * Adds the dice to used dice.
+     *
+     * @param dice the dice to add
+     */
+    public void addDiceToUsedDice(final Dice dice) {
         usedDice.add(dice);
         setBorderColor(diceViews[fishingRod.getComponents().indexOf(dice)], USED_COLOR);
     }
