330eadb46f878e47424a4924a93b090145002a17
MARTIN SIU <msiu28@my.bcit.ca>
2023-12-09 13:05:04 -0800

    Add javadocs and fix checkstyle warnings

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Dice.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202330termprojectmartincharliegame/Dice.java
@@ -1,13 +1,37 @@
 package ca.bcit.comp2522.termproject.comp2522202330termprojectmartincharliegame;
 import java.util.ArrayList;
+
+/**
+ * An interface that represents a die.
+ *
+ * @author Charlie Martin
+ * @version 2023
+ */
 public interface Dice {
+    /**
+     * Rolls the die.
+     */
     void roll();
+
+    /**
+     * Gets the face list.
+     *
+     * @return the face list
+     */
     ArrayList<Integer> getFaceList();
 
-    void removeFace(Integer face);
-    void addFace(Integer face);
-
+    /**
+     * Gets the face up value.
+     *
+     * @return the face up value
+     */
     Integer getFaceUpValue();
 
+    /**
+     * Replaces a face.
+     *
+     * @param face the face to replace
+     * @param replacementFace the face to replace it with
+     */
     void replaceFace(Integer face, Integer replacementFace);
 }
